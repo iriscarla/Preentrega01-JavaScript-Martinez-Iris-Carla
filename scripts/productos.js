674cece81a524bfd4f65dc965ac4841c2a5f985b
iriscarla <iriscarlam@gmail.com>
2023-11-18 12:06:26 -0300

    MODIF

---
+++ scripts/productos.js
@@ -0,0 +1,24 @@
+//Arrays simulando base de datos de productos a la venta.
+
+const cerverzas = [
+    { id: 1 , nombre: 'APA', precio: 1500, categoria: 'cerveza', volumenAlcohol: 3.5},
+    { id: 2 , nombre: 'IPA', precio: 1200, categoria: 'cerveza', volumenAlcohol: 4.5},
+    { id: 3 , nombre: 'Golden', precio: 1000, categoria: 'cerveza', volumenAlcohol: 2.0},
+    { id: 4 , nombre: 'Irish Red', precio: 1300, categoria: 'cerveza', volumenAlcohol: 2.7},
+    { id: 5 , nombre: 'Barley Wine', precio: 1350, categoria: 'cerveza', volumenAlcohol: 10},
+ ]
+ 
+ const papas = [
+     { id:1 , nombre: 'Papas Clásicas', precio: 2000, categoria: 'papas'},
+     { id:1 , nombre: 'Papas Americanas', precio: 2500, categoria: 'papas'},
+     { id:1 , nombre: 'Papas con Cheddar', precio: 3000, categoria: 'papas'},
+     { id:1 , nombre: 'Salchipapas', precio: 4000, categoria: 'papas'},
+ ]
+ 
+ const pizzas = [
+     { id:1 , nombre: 'MOZZARELLA', precio: 4479, categoria: 'pizzas'},
+     { id:1 , nombre: 'RUCULA Y JAMÓN CRUDO', precio: 5000, categoria: 'pizzas'},
+     { id:1 , nombre: '4 QUESOS', precio: 4800, categoria: 'pizzas'},
+     { id:1 , nombre: 'NAPOLITANA', precio: 4799, categoria: 'pizzas'},
+ ]
+ ///-----------------------------------------------ARRAYS
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,135 @@
-function elegirCartaPapas() {
-    let eleccionCliente = prompt('Puede elegir entre \n 1: Papas Clásicas \n 2. Papas de la Casa  \n 3. Papas Americanas \n 4. salchipapas \n 5: Para SALIR');
+let menuInicio = prompt('Bienvenido al bar! Puede optar por: \n a: Ver la carta de papas \n b. Catalogo de cervezas  \n c. Quiero salir  ');
+
+if (menuInicio !== undefined && menuInicio !== '') {
+    if (menuInicio.trim().toLocaleLowerCase() == 'a') {
+        mostarCartaPapas()
+    }
+    else if (menuInicio.trim().toLocaleLowerCase() == 'b') {
+        mostarCartaCervezas()
+    }
+    else if (menuInicio == 'c') {
+        alert('Chau')
+    }
+    else {
+        alert('Esperamos verte de nuevo pronto! 🎉')
+    }
+}
+
+
+//Funcion para ver la carta de papas.
+function mostarCartaPapas() {
+    let eleccionCliente = prompt('Puede elegir entre \n 1: Papas Clásicas \n 2. Papas Americanas  \n 3. Papas con Cheddar \n 4. Salchipapas \n 5: Para SALIR');
 
     if (eleccionCliente !== null || undefined || false) {
         while (eleccionCliente !== '5') {
 
             switch (eleccionCliente) {
                 case '1':
-                    alert("Ud. ordenó: Papas Clásicas $ 2300");
+                    alert("Papas Clásicas $2000");
                     break
 
                 case '2':
-                    alert("Ud. ordenó: Papas de la Casa $ 3100");
+                    alert("Papas Americanas $ 2500");
                     break
 
                 case '3':
-                    alert("Ud. ordenó: Papas Americanas $ 3000");
+                    alert("Papas con Cheddar $ 3000");
                     break
 
                 case '4':
-                    alert("Ud. ordenó: Salchipapas $3500");
+                    alert("Salchipapas $4000");
+                    break
+
+                default:
+                    alert("Elige una opción válida por favor.");
+            }
+            eleccionCliente = prompt('Puede elegir entre \n 1: Papas Clásicas \n 2. Papas Americanas  \n 3. Papas con Cheddar \n 4. Salchipapas \n 5: Para SALIR');
+        }
+
+    }
+}
+
+
+//Funcion para ver la carta de cervezas.
+ function mostarCartaCervezas() {
+    let beer = prompt('Puede elegir entre \n 1: APA \n 2. IPA  \n 3. Golden \n 4. Irish Red \n 5. Barley Wine \n 5: Para SALIR');
+    if (beer!== null || undefined || false)
+        while (beer !== '5') {
+
+            switch (beer) {
+                case '1':
+                    alert("Cerveza 'APA, Precio $1500, Volumen de alcohol: 3.5");
+                    break
+
+                case '2':
+                    alert("Cerveza 'IPA', Precio $1200, Volumen de alcohol: 4.5");
+                    break
+
+                case '3':
+                    alert("Cerveza 'GOLDEN', Precio $1000, Volumen de alcohol: 2.0");
+                    break
+
+                case '4':
+                    alert("Ud. ordenó: Cerveza Irish Red $ 1300");
+                    break
+
+                case '5':
+                    alert("Ud. ordenó: Cerveza Barley Wine $ 1350");
+                    break
+
+                default:
+                    alert("Elige una opción válida por favor.");
+            }
+            beer = prompt('Puede elegir entre \n 1: Cerveza Rubia \n 2. Cerveza Negra  \n 3. Cerveza Roja \n 5: Para SALIR');
+
+        }
+
+}
+
+// Funcion para buscar una cerveza por nombre
+function buscarCerveza (nombre){
+
+     const filtrado = cerverzas.filter((cerveza) => cerveza.nombre.includes(nombre));
+     return filtrado;
+    ;
+}
+
+
+
+
+
+// funcion para añadir al carrito
+const carrito = [];
+
+function agregarAlCarrito (){
+
+}
+
+
+
+
+
+function elegirCartaPapas() {
+    let eleccionCliente = prompt('Puede elegir entre \n 1: Papas Clásicas \n 2. Papas Americanas  \n 3. Papas con Cheddar \n 4. Salchipapas \n 5: Para SALIR');
+
+    if (eleccionCliente !== null || undefined || false) {
+        while (eleccionCliente !== '5') {
+
+            switch (eleccionCliente) {
+                case '1':
+                    alert("Ud. ordenó: Papas Clásicas $ 2000");
+                    break
+
+                case '2':
+                    alert("Ud. ordenó: Papas Americanas $ 2500");
+                    break
+
+                case '3':
+                    alert("Ud. ordenó: Papas con Cheddar $ 3000");
+                    break
+
+                case '4':
+                    alert("Ud. ordenó: Salchipapas $4000");
                     break
 
                 default:
@@ -31,32 +142,7 @@ function elegirCartaPapas() {
 }
 
 
-function elegirCartaCervezas() {
-    let beer = prompt('Puede elegir entre \n 1: Cerveza Rubia \n 2. Cerveza Negra  \n 3. Cerveza Roja \n 5: Para SALIR');
-    if (beer !== null || undefined || false)
-        while (beer !== '5') {
 
-            switch (beer) {
-                case '1':
-                    alert("Ud. ordenó: Cerveza Rubia $ 1000");
-                    break
-
-                case '2':
-                    alert("Ud. ordenó: Cerveza Negra $ 800");
-                    break
-
-                case '3':
-                    alert("Ud. ordenó: Cerveza Roja $ 1200");
-                    break
-
-                default:
-                    alert("Elige una opción válida por favor.");
-            }
-            beer = prompt('Puede elegir entre \n 1: Cerveza Rubia \n 2. Cerveza Negra  \n 3. Cerveza Roja \n 5: Para SALIR');
-
-        }
-
-}
 
 
 function eleccionEnvio() {
@@ -72,22 +158,7 @@ function eleccionEnvio() {
 
 //-----------Desde aca el inicio
 
-let menuInicio = prompt('Bienvenido al bar! Puede optar por: \n a: Ver la carta de papas \n b. Catalogo de cervezas  \n c. Quiero salir  ');
 
-if (menuInicio !== undefined && menuInicio !== '') {
-    if (menuInicio == 'a') {
-        elegirCartaPapas()
-    }
-    else if (menuInicio == 'b') {
-        elegirCartaCervezas()
-    }
-    else if (menuInicio == 'c') {
-        alert('Chau')
-    }
-    else if (menuInicio == undefined) {
-        alert('Esperamos verte de nuevo pronto! 🎉')
-    }
-}
 
 
 
